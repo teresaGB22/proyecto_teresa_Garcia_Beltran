e46e4f1753b8548fbbd809951ef5b42134f40464
Teresa Garcia <garcia.beltran.teresa.de.los.angeles@iescamas.es>
2025-05-01 18:29:44 +0200

    añadida clase Venta

--- src/clases/Administrador.java
+++ src/clases/Administrador.java
@@ -1,38 +1,47 @@
 package clases;
 
 public class Administrador {
-private int idAdministrador;
-private String usuario;
-private String contrasenya;
-public Administrador(int idAdministrador, String usuario, String contrasenya) {
-	this.idAdministrador = idAdministrador;
-	this.usuario = usuario;
-	this.contrasenya = contrasenya;
-}
-public Administrador() {
-	
-}
+	@Id
+	private int idAdministrador;
+	private String usuario;
+	private String contrasenya;
 
-public Administrador(int idAdministrador) {
-	this.idAdministrador = idAdministrador;
-}
-public int getIdAdministrador() {
-	return idAdministrador;
-}
-public void setIdAdministrador(int idAdministrador) {
-	this.idAdministrador = idAdministrador;
-}
-public String getUsuario() {
-	return usuario;
-}
-public void setUsuario(String usuario) {
-	this.usuario = usuario;
-}
-public String getContrasenya() {
-	return contrasenya;
-}
-public void setContrasenya(String contrasenya) {
-	this.contrasenya = contrasenya;
-}
+	public Administrador(int idAdministrador, String usuario, String contrasenya) {
+		this.idAdministrador = idAdministrador;
+		this.usuario = usuario;
+		this.contrasenya = contrasenya;
+	}
+
+	public Administrador() {
+
+	}
+
+	public Administrador(int idAdministrador) {
+		this.idAdministrador = idAdministrador;
+	}
+
+	public int getIdAdministrador() {
+		return idAdministrador;
+	}
+
+	public void setIdAdministrador(int idAdministrador) {
+		this.idAdministrador = idAdministrador;
+	}
+
+	public String getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+
+	public String getContrasenya() {
+		return contrasenya;
+	}
+
+	public void setContrasenya(String contrasenya) {
+		this.contrasenya = contrasenya;
+	}
 
 }
