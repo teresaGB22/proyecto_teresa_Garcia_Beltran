f907d6f2243cccb5d40994a3eaac36a4c7c6933c
Teresa Garcia <garcia.beltran.teresa.de.los.angeles@iescamas.es>
2025-05-01 17:49:38 +0200

    añadida clase Proveedor

--- src/clases/Administrador.java
+++ src/clases/Administrador.java
@@ -4,5 +4,35 @@ public class Administrador {
 private int idAdministrador;
 private String usuario;
 private String contrasenya;
+public Administrador(int idAdministrador, String usuario, String contrasenya) {
+	this.idAdministrador = idAdministrador;
+	this.usuario = usuario;
+	this.contrasenya = contrasenya;
+}
+public Administrador() {
+	
+}
+
+public Administrador(int idAdministrador) {
+	this.idAdministrador = idAdministrador;
+}
+public int getIdAdministrador() {
+	return idAdministrador;
+}
+public void setIdAdministrador(int idAdministrador) {
+	this.idAdministrador = idAdministrador;
+}
+public String getUsuario() {
+	return usuario;
+}
+public void setUsuario(String usuario) {
+	this.usuario = usuario;
+}
+public String getContrasenya() {
+	return contrasenya;
+}
+public void setContrasenya(String contrasenya) {
+	this.contrasenya = contrasenya;
+}
 
 }
